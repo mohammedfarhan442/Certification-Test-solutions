7c6dae7af7cd02dfd45653ae44b5121ffccc1f73
Mohammed Farhan <61489694+mohammedfarhan442@users.noreply.github.com>
2022-07-06 23:43:09 +0530

    Add files via upload

---
+++ 1. Hacker Rank Pyhton Basic Certification skills test solution -  Python - Vending Machine.py
@@ -0,0 +1,45 @@
+			2. Python: Vending Machine:-
+
+import math
+import os
+import random
+import re
+import sys
+
+class VendingMachine:
+    # Implement the VendingMachine here
+    def __init__(self, num_items, item_price):
+        self.num_items = num_items
+        self.items_price = item_price
+
+    def buy (self, req_items, money):
+        if req_items>self.num_items:
+            return "Not enough items in the machine"
+
+        else:
+            if money>=req_items*self.items_price:
+                self.num_items -= req_items
+                return money-req_items*self.items_price
+
+            else:
+                return "Not enough coins"
+    #pass
+
+if __name__ == '__main__':
+    
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    num_items, item_coins = map(int, input().split())
+    machine = VendingMachine(num_items, item_coins)
+
+    n = int(input())
+    for _ in range(n):
+        num_items, num_coins = map(int, input().split())
+        try:
+            change = machine.buy(num_items, num_coins)
+            fptr.write(str(change) + "\n")
+        except ValueError as e:
+            fptr.write(str(e) + "\n")
+
+
+    fptr.close()
